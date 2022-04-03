8e0f46266324fa935151ac303ff1bdb2a0bb10bb
Tasmiya-Malik <60136068+Tasmiya-Malik@users.noreply.github.com>
2022-04-03 21:34:25 +0500

    Navbar, Header, Top tile

--- ledgrr/lib/main.dart
+++ ledgrr/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:ledgrr/Screens/login/login.dart';
 import 'package:ledgrr/constraints.dart';
+import 'package:ledgrr/components/size_config.dart';
+import 'package:ledgrr/Screens/dashboard/dashboard.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,10 +16,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-          primaryColor: kPrimaryColor, scaffoldBackgroundColor: Colors.white),
-      home: LoginScreen(),
+      // title: 'Flutter Demo',
+      // theme: ThemeData(
+      //     primaryColor: kPrimaryColor, scaffoldBackgroundColor: Colors.white),
+      // home: LoginScreen(),
+      home: Dashboard(),
     );
   }
 }
